67d446c290e509b1547359a60628c032681c580a
Никита <99522484+kwttr@users.noreply.github.com>
2023-10-15 09:44:26 +0700

    Before realisation of Teacher/Hof/Admin attachments to AspNetUsers

---
+++ Models/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace workload.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
