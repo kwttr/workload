71c7651a6e139ad136880df32ee73110d397bf4a
Никита <99522484+kwttr@users.noreply.github.com>
2023-11-15 11:35:18 +0700

    f

---
+++ workload_Models/ViewModels/ProfileVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workload_Models.ViewModels
+{
+    public class ProfileVM
+    {
+        public string Role { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
